009264e8e5fb667f6346f889c3e6a6741be34f2b
Mrfleurot <fleurot.alex@gmail.com>
2017-03-16 13:00:07 +0100

    Serveur/RestFull

--- src/main/java/isiscapitalist/Services.java
+++ src/main/java/isiscapitalist/Services.java
@@ -66,33 +66,33 @@ public class Services {
 // sur lequel une action a eu lieu (lancement manuel de production ou
 // achat d’une certaine quantité de produit)
 // renvoie false si l’action n’a pas pu être traitée
-    public Boolean updateProduct(String username, ProductType newproduct) throws JAXBException {
-
-// aller chercher le monde qui correspond au joueur
-        World world = getWorld(username);
-// trouver dans ce monde, le produit équivalent à celui passé
-// en paramètre
-        ProductType product = findProductById(world, newproduct.getId());
-
-        if (product == null) {
-            return false;
-        }
-// calculer la variation de quantité. Si elle est positive c'est
-// que le joueur a acheté une certaine quantité de ce produit
-// sinon c’est qu’il s’agit d’un lancement de production.
-        int qtchange = newproduct.getQuantite() - product.getQuantite();
-
-        if (qtchange > 0) {
-// soustraire de l'argent du joueur le cout de la quantité
-// achetée et mettre à jour la quantité de product
-        } else {
-// initialiser product.timeleft à product.vitesse
-// pour lancer la production
-        }
-// sauvegarder les changements du monde
-        saveWorldToXML(world, username);
-        return true;
-
-    }
+//    public Boolean updateProduct(String username, ProductType newproduct) throws JAXBException {
+//
+//// aller chercher le monde qui correspond au joueur
+//        World world = getWorld(username);
+//// trouver dans ce monde, le produit équivalent à celui passé
+//// en paramètre
+//        ProductType product = findProductById(world, newproduct.getId());
+//
+//        if (product == null) {
+//            return false;
+//        }
+//// calculer la variation de quantité. Si elle est positive c'est
+//// que le joueur a acheté une certaine quantité de ce produit
+//// sinon c’est qu’il s’agit d’un lancement de production.
+//        int qtchange = newproduct.getQuantite() - product.getQuantite();
+//
+//        if (qtchange > 0) {
+//// soustraire de l'argent du joueur le cout de la quantité
+//// achetée et mettre à jour la quantité de product
+//        } else {
+//// initialiser product.timeleft à product.vitesse
+//// pour lancer la production
+//        }
+//// sauvegarder les changements du monde
+//        saveWorldToXML(world, username);
+//        return true;
+//
+//    }
 
 }
