8e3c69e222c753b6764954a414a1c84ed44b50e1
Mrfleurot <fleurot.alex@gmail.com>
2017-03-27 10:33:42 +0200

    Serveur/v99.1

--- src/main/java/isiscapitalist/Services.java
+++ src/main/java/isiscapitalist/Services.java
@@ -94,86 +94,61 @@ public class Services {
 
 
 // prend en paramètre le pseudo du joueur et le produit
-
 // sur lequel une action a eu lieu (lancement manuel de production ou
-
 // achat d’une certaine quantité de produit)
-
 // renvoie false si l’action n’a pas pu être traitée 
     
     public Boolean updateProduct(String username, ProductType newproduct) throws JAXBException, FileNotFoundException {
-
 // aller chercher le monde qui correspond au joueur
         World world = getWorld(username);
-
 // trouver dans ce monde, le produit équivalent à celui passé
 // en paramètre
         ProductType product = findProductById(world, newproduct.getId());
-
         if (product == null) {
             return false;
         }
-
 // calculer la variation de quantité. Si elle est positive c'est
 // que le joueur a acheté une certaine quantité de ce produit
 // sinon c’est qu’il s’agit d’un lancement de production.
         int qtchange = newproduct.getQuantite() - product.getQuantite();
-
         if (qtchange > 0) {
             product.setQuantite(product.getQuantite() + qtchange);
-
 // soustraire de l'argent du joueur le cout de la quantité
 // achetée et mettre à jour la quantité de product
         } else {
-
 // initialiser product.timeleft à product.vitesse
 // pour lancer la production
         }
-
 // sauvegarder les changements du monde
         saveWorldToXML(world, username);
-
         return true;
     }
 
 // prend en paramètre le pseudo du joueur et le manager acheté.
 // renvoie false si l’action n’a pas pu être traitée
     public Boolean updateManager(String username, PallierType newmanager) throws JAXBException, FileNotFoundException {
-
 // aller chercher le monde qui correspond au joueur
         World world = getWorld(username);
-
 // trouver dans ce monde, le manager équivalent à celui passé
 // en paramètre
         PallierType manager = findManagerByName(world, newmanager.getName());
-
         if (manager == null) {
-
             return false;
-
         }
         
         // débloquer ce manager
         manager.setUnlocked(true);
-
 // trouver le produit correspondant au manager
-
 ProductType product = findProductById(world, manager.getIdcible());
-
         if (product == null) {
-
             return false;
-
         }
-
 // débloquer le manager de ce produit
         product.setManagerUnlocked(true);
 // soustraire de l'argent du joueur le cout du manager
         world.setMoney(world.getMoney() - manager.getSeuil());
 // sauvegarder les changements au monde
         saveWorldToXML(world, username);
-
         return true;
-
     }
 }
