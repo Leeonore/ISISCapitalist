c73be5418f935e88523a3420d4cdcc4e8667811e
Mrfleurot <fleurot.alex@gmail.com>
2017-04-03 09:53:05 +0200

    Serveur/Unlock

--- src/main/java/isiscapitalist/Services.java
+++ src/main/java/isiscapitalist/Services.java
@@ -25,7 +25,8 @@ public class Services {
     private Marshaller m;
     private List<Integer> quantiteAttente = new ArrayList<Integer>(Collections.nCopies(6, 0));
 
-    /** Désérialisation du XML
+    /**
+     * Désérialisation du XML
      *
      * @param username
      * @return
@@ -38,21 +39,21 @@ public class Services {
         } catch (JAXBException e) {
             System.err.println(e.getMessage());
             this.world = null;
-        } 
+        }
         try {
-            world = (World) u.unmarshal(new File(username+"-world.xml"));
+            world = (World) u.unmarshal(new File(username + "-world.xml"));
         } catch (UnmarshalException e) {
             InputStream input = getClass().getClassLoader().getResourceAsStream("world.xml");
             world = (World) u.unmarshal(input);
         }
         UpdateScore(world);
-         return world;   
+        return world;
     }
 
     /**
      * Sérialisation du XML
      *
-     * @param world 
+     * @param world
      * @param username
      * @throws JAXBException
      * @throws java.io.FileNotFoundException
@@ -63,63 +64,72 @@ public class Services {
         m = cont.createMarshaller();
         m.marshal(world, output);
     }
-    
-    /**Charger le monde d'un joueur
+
+    /**
+     * Charger le monde d'un joueur
+     *
      * @param username
      * @return
      * @throws JAXBException
      * @throws FileNotFoundException
      */
-    public World getWorld(String username) throws JAXBException, FileNotFoundException{
+    public World getWorld(String username) throws JAXBException, FileNotFoundException {
         World world = readWorldFromXml(username);
         //Update score sur le monde toussa toussa
         UpdateScore(world);
         saveWorldToXML(world, username);
         return world;
     }
-    
-    /** Trouver un produit avec son ID
+
+    /**
+     * Trouver un produit avec son ID
+     *
      * @param world
      * @param productId
      * @return
      */
-    public ProductType findProductById(World world, int productId){
+    public ProductType findProductById(World world, int productId) {
         List<ProductType> produits = world.getProducts().getProduct();
-        for ( ProductType produit : produits){
-            if ( produit.getId()== productId){
+        for (ProductType produit : produits) {
+            if (produit.getId() == productId) {
                 return produit;
             }
         }
         return null;
     }
-    
-    /**Trouver un manager avec son nom
+
+    /**
+     * Trouver un manager avec son nom
+     *
      * @param world
      * @param managerName
      * @return
      */
-    public PallierType findManagerByName(World world, String managerName){
+    public PallierType findManagerByName(World world, String managerName) {
         List<PallierType> managers = world.getManagers().getPallier();
-        for ( PallierType manager : managers){
-            if (manager.getName().equals(managerName)){
+        for (PallierType manager : managers) {
+            if (manager.getName().equals(managerName)) {
                 return manager;
             }
         }
         return null;
     }
-    
-    /** Mettre à jour un produit (production ou achat)
+
+    /**
+     * Mettre à jour un produit (production ou achat)
+     *
      * @param username = pseudo du joueur
      * @param newproduct
-     * @return false si l’action n’a pas pu être traitée 
+     * @return false si l’action n’a pas pu être traitée
      * @throws JAXBException
      * @throws FileNotFoundException
-     */    
+     */
     public Boolean updateProduct(String username, ProductType newproduct) throws JAXBException, FileNotFoundException {
         World world = getWorld(username); // aller chercher le monde qui correspond au joueur
         ProductType product = findProductById(world, newproduct.getId()); // trouver dans ce monde, le produit passé en paramètre
-        if (product == null) return false;
-        
+        if (product == null) {
+            return false;
+        }
         int qtchange = newproduct.getQuantite() - product.getQuantite(); // calculer la variation de quantité.
         if (qtchange > 0) {// Si elle est positive, le joueur a acheté une certaine quantité
             if (product.getTimeleft() == 0) { //Si le produit n'est pas en cours de production
@@ -129,8 +139,15 @@ public class Services {
                 product.setQuantite(product.getQuantite() + qtchange); //quantite de produit
                 product.setCout(newproduct.getCout()); // cout d'un produit
             } else { // si le produit est en production
-                quantiteAttente.set(product.getId()-1, qtchange); //stocker la quantité
+                quantiteAttente.set(product.getId() - 1, qtchange + product.getQuantite()); //stocker la quantité
             }
+            List<PallierType> unlocks = product.getPalliers().getPallier();
+                for (PallierType unlock : unlocks) {
+                    if (unlock.getSeuil() <= (product.getQuantite() + qtchange) && unlock.isUnlocked() ==false) {
+                        unlock.setUnlocked(true);
+                        UpdateUnlock(product, unlock);
+                    }
+                };
         } else {// sinon c’est qu’il s’agit d’un lancement de production.
             product.setTimeleft(newproduct.getVitesse()); // initialiser product.timeleft à product.vitesse
             //world.setLastupdate(System.currentTimeMillis()); //Enregistrer la date
@@ -138,10 +155,12 @@ public class Services {
         // sauvegarder les changements du monde
         saveWorldToXML(world, username);
         return true;
-        
+
     }
 
-    /** Mettre à jour un manager (achat)
+    /**
+     * Mettre à jour un manager (achat)
+     *
      * @param username
      * @param newmanager
      * @return false si l’action n’a pas pu être traitée
@@ -151,12 +170,12 @@ public class Services {
     public Boolean updateManager(String username, PallierType newmanager) throws JAXBException, FileNotFoundException {
         World world = getWorld(username); // aller chercher le monde qui correspond au joueur
         PallierType manager = findManagerByName(world, newmanager.getName()); // trouver dans ce monde, le manager équivalent passé en parametre
-        
+
         if (manager == null) {
             return false;
         }
         manager.setUnlocked(true); // débloquer ce manager
-        
+
         ProductType product = findProductById(world, manager.getIdcible()); // trouver le produit correspondant au manager
         if (product == null) {
             return false;
@@ -167,30 +186,33 @@ public class Services {
         saveWorldToXML(world, username); // sauvegarder les changements au monde
         return true;
     }
-    
-    /** Mettre à jour le score
+
+    /**
+     * Mettre à jour le score
+     *
      * @param world
      */
-        public void UpdateScore(World world) {
+    public void UpdateScore(World world) {
         long duree = System.currentTimeMillis() - world.getLastupdate(); //calculé durée depuis derniere mise à jour
         List<ProductType> produits = world.getProducts().getProduct();
         for (ProductType product : produits) {
-            if (product.isManagerUnlocked()){ //Si manager 
+            if (product.isManagerUnlocked()) { //Si manager 
                 long nbProduit = duree / product.getVitesse(); //Nombre de produit créé, fini
                 world.setMoney(world.getMoney() + (product.getRevenu() * product.getQuantite() * nbProduit));// Ajouter le revenu
                 duree = duree % product.getVitesse(); //Calcul du temps restant
             }
-                CalculScore(product, duree);
+            CalculScore(product, duree);
         }
-    world.setLastupdate(System.currentTimeMillis()); //Enregistrer la date de la derniere mise à jour
+        world.setLastupdate(System.currentTimeMillis()); //Enregistrer la date de la derniere mise à jour
     }
-    
+
     public void CalculScore(ProductType product, long duree) {
-        if (product.getTimeleft() == 0) {}//Si la production n'est pas en cours
+        if (product.getTimeleft() == 0) {
+        }//Si la production n'est pas en cours
         //Si la production est en cours
-        else { 
+        else {
             product.setTimeleft(product.getTimeleft() - duree); //Mettre à jour le timeleft
-            if (product.getTimeleft() - duree <= 0) { //Si la production est finie
+            if (product.getTimeleft() <= 0) { //Si la production est finie
                 product.setTimeleft(0); //Remettre le timeleft à 0
                 world.setMoney(world.getMoney() + (product.getRevenu() * product.getQuantite())); //Mettre à jour money
                 product.setQuantite(product.getQuantite() + quantiteAttente.get(product.getId() - 1)); //Mettre à jour quantité si achat pendant production
@@ -200,5 +222,24 @@ public class Services {
             }
 
         }
+    }
+
+    public void UpdateUnlock(ProductType product, PallierType objet) {
+        //Si type gain
+        if (objet.getTyperatio().value().equals("gain")) {
+            //Mettre à jour le revenu
+            product.setRevenu(product.getRevenu() * objet.getRatio());
+            //Si type vitesse
+        } else if (objet.getTyperatio().value().equals("vitesse")) {
+            //Mettre à jour la vitesse et le timeleft
+            product.setVitesse(product.getVitesse() / (int) objet.getRatio()); //la vitesse de production
+            product.setTimeleft(product.getTimeleft() / (int) objet.getRatio()); //le temps restant
+            // Si type ange
+        } else {
+            if (world.getActiveangels() > 0) { //si il y a des anges actifs
+                //On met à jour l'angel bonus
+                world.setAngelbonus(world.getAngelbonus() + (int) objet.getRatio() * (int) world.getActiveangels());
+            }
+        };
     }
 }
